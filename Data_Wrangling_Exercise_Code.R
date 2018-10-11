692d07859a8888b5d9a1b070b0dae28b2244b953
YOUR FULL NAME <SCHULTZRYNE@GMAIL.COM>
2018-10-11 14:14:41 -0400

    2: Separate product code and number

--- Data_Wrangling_Exercise_Code.R
+++ Data_Wrangling_Exercise_Code.R
@@ -1,3 +1,9 @@
 Data <- read.csv("refine_original.csv")
 
 Data$company <- tolower(Data$company)
+
+Data$Product.code...number <- as.character(Data$Product.code...number)
+str(Data)
+
+library(tidyr)
+Data <- separate(Data, Product.code...number, c("Product.code", "Product.number"), sep = "-", remove = TRUE)

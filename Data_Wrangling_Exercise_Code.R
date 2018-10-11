04532d86b4fd2428f5bbb69e2995236cf2dd8850
YOUR FULL NAME <SCHULTZRYNE@GMAIL.COM>
2018-10-11 15:33:37 -0400

    3: Add product categories

--- Data_Wrangling_Exercise_Code.R
+++ Data_Wrangling_Exercise_Code.R
@@ -6,3 +6,6 @@ Data$Product.code...number <- as.character(Data$Product.code...number)
 
 library(tidyr)
 Data <- separate(Data, Product.code...number, c("Product.code", "Product.number"), sep = "-", remove = TRUE)
+
+Product.category <- c("p" = "Smartphone", "v" = "TV", "x" = "Laptop", "q" = "Tablet")
+Data$Product.category <- Product.category[Data$Product.code]

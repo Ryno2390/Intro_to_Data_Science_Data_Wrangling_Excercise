87f76ac1512c6551cee98f9b18644cd00bf54eda
YOUR FULL NAME <SCHULTZRYNE@GMAIL.COM>
2018-10-11 14:53:17 -0400

    Cleaning up the data

--- Data_Wrangling_Exercise_Code.R
+++ Data_Wrangling_Exercise_Code.R
@@ -3,7 +3,6 @@ Data <- read.csv("refine_original.csv")
 Data$company <- tolower(Data$company)
 
 Data$Product.code...number <- as.character(Data$Product.code...number)
-str(Data)
 
 library(tidyr)
 Data <- separate(Data, Product.code...number, c("Product.code", "Product.number"), sep = "-", remove = TRUE)
